f150401cd56eb758caea59a7a5f3042cb6ab8a50
unknown <chiragsethiya123@gmail.com>
2023-12-14 03:15:44 +0530

    added react router to project ui component

--- employee-management-system-ui/src/App.js
+++ employee-management-system-ui/src/App.js
@@ -2,14 +2,23 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import AddEmployee from './components/AddEmployee';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import EmployeeList from './components/EmployeeList';
 
 
 function App() {
   return (
-    <div>
+    <>
+    <BrowserRouter>
       <Navbar/>
-      <AddEmployee/>
-    </div>
+      <Routes>
+        <Route index element={<EmployeeList />}></Route>
+        <Route path='/' element={<EmployeeList />}></Route>
+        <Route path='/employeeList' element={<EmployeeList />}></Route>
+        <Route path='/addEmployee' element={<AddEmployee />}></Route>
+      </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
