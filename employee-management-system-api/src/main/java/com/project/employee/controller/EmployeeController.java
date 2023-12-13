7c340cd9779851850acd5d8279a4a96cffc71480
unknown <chiragsethiya123@gmail.com>
2023-12-14 02:34:33 +0530

    added axios, updated db credentials, and added service to saveEmployee to database

--- employee-management-system-api/src/main/java/com/project/employee/controller/EmployeeController.java
+++ employee-management-system-api/src/main/java/com/project/employee/controller/EmployeeController.java
@@ -3,11 +3,9 @@ package com.project.employee.controller;
 import com.project.employee.model.Employee;
 import com.project.employee.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin( origins = "http://localhost:3000/")
 @RestController
 @RequestMapping("api/v1/")
 public class EmployeeController {
